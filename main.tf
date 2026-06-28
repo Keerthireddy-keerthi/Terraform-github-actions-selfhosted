faef8c517dd8c4ce09686fb9951fc75cf8d0a060
Keerthireddy <puthalapattukeerthireddy9@gmail.com>
2026-06-28 20:46:18 +0530

    Update AWS instance AMI and type

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+    ami = "aami-08f44e8eca9095668"
+    instance_type = "t3.micro"
     tags = {
       Name = "Kheer"
     }
