a2e0ce66c0357fae515c6db63333d61af2af5b43
Keerthireddy <puthalapattukeerthireddy9@gmail.com>
2026-06-28 20:49:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "aami-08f44e8eca9095668"
+    ami = "ami-08f44e8eca9095668"
     instance_type = "t3.micro"
     tags = {
       Name = "Kheer"
